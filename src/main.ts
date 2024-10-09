a724f9e7e474bb7e8b71acf6487b9386dd40f1ca
jackson <jackson.mclane20@gmail.com>
2024-10-09 12:46:05 -0700

    added upgrade button, step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -9,29 +9,55 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement("button");
-button.innerHTML = "🗿";
-app.append(button);
+const moyaiClick = document.createElement("button");
+moyaiClick.innerHTML = "🗿";
+app.append(moyaiClick);
 
 const counter = document.createElement("div");
 counter.innerHTML = `0 Moyais`;
 app.append(counter);
 
+const upgrade = document.createElement("button");
+upgrade.innerHTML = `Upgrade`;
+upgrade.disabled = true;
+app.append(upgrade);
+
 let moyais: number = 0;
 
-button.onclick = () => {
+moyaiClick.onclick = () => {
   moyais++;
   counter.innerHTML = `${moyais.toFixed(2)} Moyais`;
 };
 
-const growthRate = .0001;
+upgrade.onclick = () => {
+  if (moyais >= 10) {
+    moyais -= 10;
+    growthRate += 0.0001;
+  }
+};
+
+let growthRate: number = 0.0;
 
 let lastFrame = performance.now();
-requestAnimationFrame(increaseMoyais);
+requestAnimationFrame(tick);
+
+function tick(){
+  increaseMoyais();
+  checkUpgrade();
+  requestAnimationFrame(tick);
+}
+
+function checkUpgrade() {
+  if(moyais >= 10) {
+    upgrade.disabled = false;
+  } else {
+    upgrade.disabled = true;
+  }
+}
+
 function increaseMoyais() {
   const deltaTime = performance.now() - lastFrame;
   lastFrame = performance.now();
   moyais += deltaTime * growthRate;
   counter.innerHTML = `${moyais.toFixed(2)} Moyais`;
-  requestAnimationFrame(increaseMoyais);
 }
